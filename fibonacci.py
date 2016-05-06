a464a958936bcce3e65614c03b494fb3fd0dd3e3
balag3 <carbalage@gmail.com>
2016-05-06 14:49:35 +0200

    Comments added to the code.

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,13 @@
 
+##Print user given count of fibonacci numbers
+
+#setting the starting numbers.Option 2  is [1,1]
 starting_numbers = [0,1]
 
+#asking user input for upper count of fibonacci numbers
 stop = input('How many fibonacci numbers do you want?')
 
+#fill the list recursively up to the given count
 def fill_fibonaccis(aList,aStop):
     if len(aList) < int(aStop) :
         new = aList[len(aList)-1] + aList[len(aList)-2]
@@ -13,7 +18,7 @@ def fill_fibonaccis(aList,aStop):
 
 genarated = fill_fibonaccis(starting_numbers,stop)
 
-
+# printing lines with the numbercount
 def pretty_print(aList):
     line = 1
     for i in aList:
